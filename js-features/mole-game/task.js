a6b3d0e5847cec9b0c62d28e1fe517dcf1bc9470
cpb-home <cpb@bizb.ru>
2023-11-24 14:31:50 +0300

    fixed mole task

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,10 +4,47 @@ const miss = document.getElementById('lost');
 for (let i = 1; i < 10; i++) {
   const hole = document.getElementById('hole'+i);
   hole.onclick = function() {
-    if (hole.classList.contains('hole_has-mole')) {
+    hole.classList.contains('hole_has-mole') ? up() : down();
+  }
+}
+
+function up() {
+  if (bit.textContent < 9) {
+    bit.textContent = Number(bit.textContent) + 1;
+  } else {
+    if (bit.textContent < 10) {
       bit.textContent = Number(bit.textContent) + 1;
+    }
+    if (confirm(`Вы набрали ${bit.textContent} очков и победили. Хотите сыграть снова?`)) {
+      restart();
     } else {
-      miss.textContent = Number(miss.textContent) + 1;
+      for (let i = 1; i < 10; i++) {
+        const hole = document.getElementById('hole'+i);
+        hole.onclick = null;
+      }
     }
   }
+}
+
+function down() {
+  if (miss.textContent < 4) {
+    miss.textContent = Number(miss.textContent) + 1;
+  } else {
+    if (miss.textContent < 5) {
+      miss.textContent = Number(miss.textContent) + 1;
+    }
+    if (confirm(`Вы проиграли. Хотите сыграть снова?`)) {
+      restart();
+    } else {
+      for (let i = 1; i < 10; i++) {
+        const hole = document.getElementById('hole'+i);
+        hole.onclick = null;
+      }
+    }
+  }
+}
+
+function restart() {
+  bit.textContent = 0;
+  miss.textContent = 0;
 }
