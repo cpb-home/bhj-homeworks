44806ef02e806bf1abc5184fa29865306e316aed
cpb-home <cpb@bizb.ru>
2023-12-05 19:02:36 +0300

    reveal task done

---
+++ dom/reveal/task.js
@@ -0,0 +1,13 @@
+const taskDivs = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  taskDivs.forEach(el => {
+    const {top, bottom} = el.getBoundingClientRect();
+
+    if (bottom < 0 || top > window.innerHeight) {
+      el.classList.remove('reveal_active');
+    } else {
+      el.classList.add('reveal_active');
+    }
+  });
+})
