5bc9e5aa1638df099811a25af7e4831e488df130
cpb-home <cpb@bizb.ru>
2023-12-09 17:14:09 +0300

    todo task done

---
+++ document-structure/todo/task.js
@@ -0,0 +1,80 @@
+const 
+  addBtn = document.querySelector('.tasks__add'),
+  input = document.getElementById('task__input'),
+  tasksList = document.querySelector('.tasks__list');
+
+getFromLS();
+
+addBtn.addEventListener('click', addEvent => {
+  addEvent.preventDefault();
+  if (input.value != '') {
+    addTask(input.value);
+    input.value = '';
+  }
+});
+
+
+
+function addTask(inpVal) {
+  const
+    task = document.createElement('div'),
+    taskTitle = document.createElement('div'),
+    taskRemove = document.createElement('a');
+
+  task.className = 'task';
+  taskTitle.className = 'task__title';
+  taskRemove.className = 'task__remove';
+
+  taskTitle.textContent = inpVal;
+  taskRemove.innerHTML = '&times;';
+
+  task.appendChild(taskTitle);
+  task.appendChild(taskRemove);
+  tasksList.appendChild(task);
+
+  addCloseListner();
+  addToLS();
+}
+
+function removeTask(ev, e) {
+  const parent = ev.target.closest('.task');
+  parent.remove();
+  e.removeEventListener('click', ev => removeTask(ev, e));
+  localStorage.removeItem('task' + parent.dataset.lsnum);
+}
+
+function addCloseListner() {
+  const closeBtns = document.querySelectorAll('.task__remove');
+
+  closeBtns.forEach(e => {
+    e.addEventListener('click', ev => removeTask(ev, e));
+  });
+}
+
+function getFromLS() {
+  //localStorage.setItem("myCat1", "Tom");
+  //localStorage.setItem("myCat2", "Toma");
+  //let cat = localStorage.getItem("myCat1");
+  //localStorage.removeItem("myCat");
+  //localStorage.clear();
+  console.log(localStorage);
+  let entriesLS = Object.entries(localStorage);
+  let arrLS = entriesLS.filter(ele => ele[0].includes('task'));
+
+  arrLS.sort().forEach(entry => {
+    localStorage.removeItem(entry[0]);
+    addTask(entry[1]);
+  });
+  
+  console.log(arrLS);
+  console.log(localStorage);
+}
+
+function addToLS() {
+  const allTasks = document.querySelectorAll('.task__title');
+
+  for(let i = 0; i < allTasks.length; i++) {
+    localStorage.setItem('task'+i, allTasks[i].textContent);
+    allTasks[i].parentNode.dataset.lsnum = i;
+  }
+}
